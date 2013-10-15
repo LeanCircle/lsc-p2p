211d918161dea710cf695427f10e4b769e10044d
Alessandro Prioni <alessandroprioni@gmail.com>
2013-10-15 15:19:26 +0200

    Finish static pages layout

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,9 @@
 module StaticPagesHelper
+
+	def nav_link(link_text, link_path)
+		content_tag(:li, class: ('active' if current_page?(link_path))) do
+			link_to link_text, link_path
+		end
+	end
+	
 end
