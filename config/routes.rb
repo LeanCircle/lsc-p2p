0b12f9c474c680811fe73aebb40caf66277af80d
Alessandro Prioni <alessandroprioni@gmail.com>
2013-10-15 14:01:26 +0200

    Add StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 P2pc::Application.routes.draw do
+  root 'static_pages#home'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/team', to: 'static_pages#team', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
