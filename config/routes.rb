8509841532b3c60bbb4f572b43bed1efe7820cdd
Tristan Kromer <TK@TristanKromer.com>
2013-11-20 15:01:52 -0800

    Refactor ugly match in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ P2pc::Application.routes.draw do
   match '/thanks', to: 'static_pages#thanks', via: :get
   match '/privacy-policy', to: 'static_pages#privacy', via: :get, as: :privacy
 
-  resources :subscribers, only: [:create]
-  match '/subscribers/thanks', to: 'subscribers#thanks', via: :get, as: :subscribers_thanks
+  resources :subscribers, only: [:create]  do
+    get 'thanks', on: :collection
+  end
 
   resources :peers, only: [:new, :create, :update] do
       get 'registration', on: :member
