7cf168d8a40195539c91a1963fa4e14a53c3d3a3
Tristan Kromer <TK@TristanKromer.com>
2013-11-20 12:26:26 -0800

    Add a route for thanks.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ P2pc::Application.routes.draw do
   match '/thanks', to: 'static_pages#thanks', via: :get
   match '/privacy-policy', to: 'static_pages#privacy', via: :get, as: :privacy
 
-  resources :subscribers, only: [:create, :thanks]
+  resources :subscribers, only: [:create]
+  match '/subscribers/thanks', to: 'subscribers#thanks', via: :get, as: :subscribers_thanks
 
   resources :peers, only: [:new, :create, :update] do
       get 'registration', on: :member
