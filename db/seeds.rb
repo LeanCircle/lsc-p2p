cecd31675c03f13016131c87eb7e493fdd150a36
Tristan Kromer <TK@TristanKromer.com>
2013-11-22 19:41:08 -0800

    Add some more folks to the volunteer list!

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,28 @@ end
 
 # Add some volunteers
 [ { name: "Zac Halbert", email: "zachalbert@gmail.com" },
+  { name: "Alessandro Prioni", email: "alessandroprioni@gmail.com" },
+  { name: "Nick Noreña", email: "ninorena@gmail.com" },
+  { name: "Karl Shaikh", email: "karl.shaikh@gmail.com" },
+  { name: "Shilpa Pulipaka", email: "shilpa@shilpapulipaka.com" },
+  { name: "Daniil Brodovich", email: "dbrodovich@gmail.com" },
+  { name: "Thomas Rampelberg", email: "thomas@saunter.org" },
+  { name: "Dan Stiefel", email: "danstiefel@gmail.com" },
+  { name: "Aaron Brown", email: "aaron.w.brown13@gmail.com" },
+  { name: "Fardeen Rahaman", email: "fardeenrahaman@gmail.com" },
+  { name: "Jeffrey Lorton", email: "jslorton@mac.com" },
+  { name: "Eric Bell", email: "eric@ericbell.info" },
+  { name: "Monique Barbanson", email: "mbarbanson@gmail.com" },
+  { name: "Nikolaus Graf", email: "nik@blossom.io" },
+  { name: "Josh Liu", email: "josh@acrossio.com" },
+  { name: "Andreas Klinger", email: "ak@garmz.com" },
+  { name: "Tim Dujon Rosenblatt", email: "tim@cloudspace.com" },
+  { name: "James Bond", email: "james.bond.home@gmail.com" },
+  { name: "Edith Harbaugh", email: "edith.harbaugh@gmail.com" },
+  { name: "Keith Ratner", email: "keith.ratner@gmail.com" },
+  { name: "Rich Collins", email: "richcollins@gmail.com" },
+  { name: "Hiten Shah", email: "hnshah@gmail.com" },
+  { name: "Sean K Murphy", email: "skmurphy@skmurphy.com" },
   { name: "Josh Liu", email: "josh@acrossio.com" }].each do |user|
   volunteer = User.find_by_email(user[:email]) || User.create(name: user[:name], email: user[:email])
   volunteer.make_volunteer
