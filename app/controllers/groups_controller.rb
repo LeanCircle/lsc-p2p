8f9cb5d726b838f0898673acc80be62266b1d1a3
Tristan Kromer <TK@TristanKromer.com>
2013-12-30 09:09:21 -0800

    Add new group page.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,4 +4,33 @@ class GroupsController < ApplicationController
     @groups = Group.approved
   end
 
+  def new
+    @group = Group.new
+  end
+
+  def create
+    redirect_to groups_path if params[:commit].eql?('Cancel')
+    @group = Group.new(post_params)
+    if @group.try(:save)
+      flash[:success] = "Your group still needs to be approved. A human will check it shortly!"
+      redirect_to groups_path
+    else
+      render :action => "new"
+    end
+  end
+
+  private
+    def post_params
+      params.require(:group).permit(:name,
+                                    :city,
+                                    :province,
+                                    :country,
+                                    :meetup_link,
+                                    :facebook_link,
+                                    :googleplus_link,
+                                    :linkedin_link,
+                                    :twitter_link,
+                                    :other_link)
+    end
+
 end
