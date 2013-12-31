c6cf3abbcc836a94daebe56c80b5305b8d1adc51
Tristan Kromer <TK@TristanKromer.com>
2013-12-30 21:24:07 -0800

    Update AA user. Has an issue where subscription is not updating. Not sure what's going on there.

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -2,10 +2,19 @@ ActiveAdmin.register User do
   permit_params :email, :password, :password_confirmation
 
   index do
-    column :email
-    column :current_sign_in_at
+    column :name, sortable: :name do |user|
+      link_to user.name, admin_user_path(user)
+    end
+    column :email, sortable: :email do |user|
+      link_to user.email, "mailto:"+user.email
+    end
+    column :newsletter_subscription, sortable: :newsletter_subscription do |user|
+      user.newsletter_subscription? ? status_tag( "True", :ok ) : status_tag( "False" )
+    end
+    column :stripe_customer_id, sortable: :stripe_customer_id do |user|
+      user.stripe_customer_id? ? status_tag( "Yes", :ok ) : status_tag( "No" )
+    end
     column :last_sign_in_at
-    column :sign_in_count
     default_actions
   end
 
@@ -14,6 +23,7 @@ ActiveAdmin.register User do
   form do |f|
     f.inputs "Admin Details" do
       f.input :email
+      f.input :newsletter_subscription, :as => :radio # TODO: This doesn't seem to be working.
       f.input :password
       f.input :password_confirmation
     end
