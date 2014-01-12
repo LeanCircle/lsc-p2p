968acba8f0d93e6558b8bcf9cccac65e356e0647
Tristan Kromer <TK@TristanKromer.com>
2014-01-12 11:58:37 -0800

    Update show form.

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  permit_params :email, :newsletter_subscription, :password, :password_confirmation, :roles
+  permit_params :email, :newsletter_subscription, :password, :password_confirmation
 
   index do
     column :name, sortable: :name do |user|
@@ -20,6 +20,28 @@ ActiveAdmin.register User do
     end
     column :last_sign_in_at
     default_actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row :email
+      row :roles do |user|
+        user.roles.map { |role| link_to(role.name, admin_role_path(role)) }.join(', ').html_safe
+      end
+      row :newsletter_subscription, sortable: :newsletter_subscription do |user|
+            user.newsletter_subscription? ? status_tag( "True", :ok ) : status_tag( "False" )
+          end
+      row :stripe_customer_id, sortable: :stripe_customer_id do |user|
+        user.stripe_customer_id? ? status_tag( "Yes", :ok ) : status_tag( "No" )
+      end
+      row :sign_in_count
+      row :last_sign_in_at
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
   end
 
   filter :email
