27bc2cb326eb3fd460745835fd2f38b17038d41d
Alessandro Prioni <alessandroprioni@gmail.com>
2014-01-18 17:18:12 +0100

    Fix peers signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :stripe_customer_id, :newsletter_subscription)
+      params.require(:user).permit(:name, :email, :password, :stripe_customer_id, :newsletter_subscription)
     end
 end
