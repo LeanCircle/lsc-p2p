7953c3860a7778b06d9d3d29160882a9410aee3a
Tristan Kromer <TK@TristanKromer.com>
2014-09-27 23:34:31 -0700

    Must be admin to edit or destroy.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,11 @@ class Ability
   def initialize(user)
     user ||= User.new
     can :manage, :all if user.is? :admin
+    if user.is? :peer
+      can :manage, :posts, :user_id => user.id
+    end
     can :read, ActiveAdmin::Page, :name => "Dashboard"
+    can :read, :all
   end
 
 end
