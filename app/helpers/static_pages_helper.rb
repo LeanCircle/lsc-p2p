384b04a4753ad5a6f0aed6e263048ae0187d0b56
Tristan Kromer <TK@TristanKromer.com>
2014-09-28 22:26:20 -0700

    Use js to highlight active link rather than helper.

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,9 +1,2 @@
 module StaticPagesHelper
-
-	def nav_link(link_text, link_path, target=nil)
-		content_tag(:li, class: ('active' if current_page?(link_path))) do
-			link_to link_text, link_path, target: target
-		end
-	end
-	
 end
