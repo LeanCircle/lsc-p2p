0a52a8b238118513b4a663770d97461bd7fa1519
Tristan Kromer <TK@TristanKromer.com>
2014-09-29 09:53:57 -0700

    Keep peers model. (for now)

--- app/models/peer.rb
+++ app/models/peer.rb
@@ -1,37 +1,7 @@
 class Peer < ActiveRecord::Base
 
-  include Common
-  attr_accessor :current_step
-  
   belongs_to :user
 
-  def current_step
-    @current_step || steps.first
-  end
-
-  def steps
-    %w[availability startup runaway]
-  end
-
-  def next_step
-    @current_step = steps[steps.index(current_step)+1]
-  end
-
-  def previous_step
-    @current_step = steps[steps.index(current_step)-1]
-  end
-
-  def first_step?
-    current_step == steps.first
-  end
-
-  def last_step?
-    current_step == steps.last
-  end
-
-  protected
-    def role_id
-      "1"
-    end
+  # Keeping this model for the data for the moment.
 
 end
