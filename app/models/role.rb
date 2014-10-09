3e27bdb7d52fc0940ef6e4bb7e9e2cc6d1b57ba2
Tristan Kromer <TK@TristanKromer.com>
2014-10-08 17:20:11 -0700

    Make a moderators method to find all moderators that will receive the daily email! (temporary method)

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,4 +2,11 @@ class Role < ActiveRecord::Base
   has_many :role_assignments
   has_many :users, through: :role_assignments
   belongs_to :user
+
+  def self.moderators
+    Role.find_by_name("admin").users +
+    Role.find_by_name("volunteer").users +
+    Role.find_by_name("organizer").users +
+    Role.find_by_name("team_member").users
+  end
 end
