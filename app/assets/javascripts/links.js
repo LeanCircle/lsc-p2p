7adf10956f9eb5bf605e8860e46cce0f0b09dfd0
Tristan Kromer <TK@TristanKromer.com>
2015-02-05 16:33:57 -0800

    Iframes are working thanks to Ryan's code.

---
+++ app/assets/javascripts/links.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+      // Grab the iFrame from above
+      var the_iframe = $("iframe#preview");
+
+      // Function to resize iFrame we grabbed
+      function resize_iframe () {
+        var viewportHeight = $(window).height();
+        // Subtract height of "top bar"
+        the_iframe.css({ height : viewportHeight - 80 })
+      }
+
+      // Resize the iFrame when the page loads
+      resize_iframe();
+
+      // Also, esize iFrame when the window is resized
+      $(window).resize(resize_iframe);
+
+      // #########################
+      // ##### Begin Content #####
+      // #########################
+
+      // Proxy Server
+      // Change this to whatever you like
+      // A local copy on your current computer
+      // or a global one on a domain
+      var proxy_host = url_to(root);
+});
